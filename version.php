7c89f9b621c1b4584d74b463aca114f7e44baaf7
PatElvisEmulator <namphong28042010@gmail.com>
2023-07-01 14:03:10 +0700

    Mô tả commit của bạn

---
+++ version.php
@@ -0,0 +1,18 @@
+<?php 
+$OC_Version = array(26,0,3,2);
+$OC_VersionString = '26.0.3';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '25.0' => true,
+    '26.0' => true,
+  ),
+  'owncloud' => 
+  array (
+    '10.11' => true,
+  ),
+);
+$OC_Build = '2023-06-22T10:04:20+00:00 5348e530ea6c61e443cf4f5cab7f4ed97ddd14d3';
+$vendor = 'nextcloud';
